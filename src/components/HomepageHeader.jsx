35468055f0db939701b1f22aa5f938dd49fd5f09
Jakin Stahl <jakinstahl@gmail.com>
2024-11-14 12:35:05 -0700

    Header majorly complete

--- src/components/HomepageHeader.jsx
+++ src/components/HomepageHeader.jsx
@@ -39,11 +39,11 @@ function HomepageHeader() {
     //   </div>
     // </div>
 
-    <Navbar expand="lg" className="bg-gradient">
+    <Navbar expand="lg" className="bg-gradient border-bottom border-gray-150 border-2">
       <Navbar.Brand>
         <img src={logo} alt="SpanishDex Logo" style={{height: '3.125rem'}}/>
       </Navbar.Brand>      
-      <Navbar.Toggle aria-controls="homepage-nav" />
+      <Navbar.Toggle aria-controls="homepage-nav" className='border-0'/>
       <Navbar.Collapse id="homepage-nav">
         <Nav className="ms-auto my-2 column-gap-25 row-gap-3">
           <Nav.Link className="ms-auto"><Button variant="outline-primary">Create Account</Button></Nav.Link>
