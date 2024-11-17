4d893781f9e28b26276d4b86b0cc80c5b89fa323
Jakin Stahl <jakinstahl@gmail.com>
2024-11-17 08:22:42 -0700

    Completed building componenets for homepage sections

--- src/components/HomepageHeader.jsx
+++ src/components/HomepageHeader.jsx
@@ -1,44 +1,10 @@
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import logo from '../assets/logo.svg';
 
 function HomepageHeader() {
   return (
-    // <Navbar expand="lg" className="bg-gradient">
-    //   <Container>
-    //     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="me-auto">
-    //         <Nav.Link href="#home">Home</Nav.Link>
-    //         <Nav.Link href="#link">Link</Nav.Link>
-    //         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-    //           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-    //           <NavDropdown.Item href="#action/3.2">
-    //             Another action
-    //           </NavDropdown.Item>
-    //           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-    //           <NavDropdown.Divider />
-    //           <NavDropdown.Item href="#action/3.4">
-    //             Separated link
-    //           </NavDropdown.Item>
-    //         </NavDropdown>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </Container>
-    // </Navbar>
-
-    // <div className="d-flex align-items-center justify-content-between bg-gradient py-4 px-50 border-bottom border-gray-150 border-2">
-    //   <img src={logo} alt="SpanishDex Logo" style={{height: '3.125rem'}}/>
-    //   <div className="d-flex align-items-center column-gap-25">
-    //     <Button variant="outline-primary">Create Account</Button>
-    //     <Button variant="primary">Log In</Button>
-    //   </div>
-    // </div>
-
     <Navbar expand="lg" className="bg-gradient border-bottom border-gray-150 border-2">
       <Navbar.Brand>
         <img src={logo} alt="SpanishDex Logo" style={{height: '3.125rem'}}/>
