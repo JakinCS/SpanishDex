36d77bf557b4c744d8c4869e40e08098d57d726c
Jakin Stahl <jakinstahl@gmail.com>
2024-11-18 19:53:22 -0700

    Fixed bug with column section overflowing

--- src/components/HomepageSectionColumns.jsx
+++ src/components/HomepageSectionColumns.jsx
@@ -7,8 +7,8 @@ import Stack from 'react-bootstrap/Stack'
 
 function HomepageSectionColumns({py, backgroundColor, image, imageBorder, imageAlt, imagePosition, headingText, children}) {
     return (
-        <Container fluid as='section' className={`px-5 py-${py} bg-${backgroundColor}`}>
-            <Row className='gx-40 gy-5 gx-xl-80 gx-xxl-100 justify-content-center'>
+        <Container fluid as='section' className={`px-5 py-${py} bg-${backgroundColor} overflow-hidden`}>
+            <Row className='gy-5 gx-40 gx-xl-80 gx-xxl-100 justify-content-center'>
                 <Col xs={{span: 12, order: 0}} sm='11' md='10' lg={{span: '6', order: imagePosition === 'left' ? 0 : 1}} xl='5' className='d-flex flex-column flex-lg-row align-items-center'>
                     <h2 className="mx-auto d-block d-lg-none text-center mb-5">{headingText}</h2>
                     <Image src={image} fluid className={imageBorder ? "border border-4 rounded" : ""} alt={imageAlt}/>
