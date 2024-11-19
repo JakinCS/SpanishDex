d4ad19a7b5ccfc64a2470693da846265703bbcd8
Jakin Stahl <jakinstahl@gmail.com>
2024-11-19 08:59:45 -0700

    added mobile layout styling for footer

--- src/components/HomepageFooter.jsx
+++ src/components/HomepageFooter.jsx
@@ -1,19 +1,28 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 // Icons
 import mail from '../assets/icons/mail.svg';
 import linkedin from '../assets/icons/linkedin.svg'
 
 function HomepageFooter() {
   return (
-    <Navbar expand="lg" className="bg-gradient border-top border-gray-150 border-2">
-        <p className='fs-5'>Copyright © 2024 SpanishDex &nbsp;•&nbsp;  <span className='fw-medium'>Created by Jakin Stahl</span></p>
-        <Nav className="ms-auto column-gap-20">
+    <Container fluid className="py-4 px-50 bg-gradient border-top border-gray-150 border-2">
+      <Row className='gy-4'>
+        <Col xs='12' md='auto' className='d-flex align-items-center'>
+          <p className='fs-5'>Copyright © 2024 SpanishDex &nbsp;•&nbsp;  <span className='fw-medium text-nowrap'>Created by Jakin Stahl</span></p>
+        </Col>
+        <Col className='col-auto ms-md-auto'>
+          <Nav className="ms-auto column-gap-20">
             <Nav.Link href="mailto:jakinstahl@gmail.com"><Image height={30} src={mail} alt='Mail icon'></Image></Nav.Link>
             <Nav.Link href="https://www.linkedin.com/in/jakinstahl" target='_blank'><Image height={30} src={linkedin} alt='LinkedIn icon'></Image></Nav.Link>
-        </Nav>
-    </Navbar>
+          </Nav>
+        </Col>
+      </Row>
+    </Container>      
   );
 }
 
