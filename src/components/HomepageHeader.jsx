7e15897bca5437b5282773c03aa51f8999f38e45
Jakin Stahl <jakinstahl@gmail.com>
2024-11-21 20:09:03 -0700

    Added menu icon toggle to close icon on click

--- src/components/HomepageHeader.jsx
+++ src/components/HomepageHeader.jsx
@@ -3,22 +3,27 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import logo from '../assets/logo.svg';
 
+import { useState } from 'react';
+
 function HomepageHeader() {
+
+  const [menuOpenState, setMenuOpenState] = useState('closed')
+  
+  const toggleMenuOpenState = () => {
+    menuOpenState === 'open' ? setMenuOpenState('closed') : setMenuOpenState('open');
+  }
+
   return (
     <Navbar expand="lg" fixed='top' className="bg-gradient border-bottom border-gray-150 border-2">
       <Navbar.Brand>
         <img src={logo} alt="SpanishDex Logo" style={{height: '3.125rem'}}/>
       </Navbar.Brand>      
-      <Navbar.Toggle aria-controls="homepage-nav" className='border-0'/>
+      <Navbar.Toggle onClick={toggleMenuOpenState} aria-controls="homepage-nav" className={(menuOpenState === 'open' ? 'show-close-icon' : 'show-menu-icon') + ' border-0'}/>
       <Navbar.Collapse id="homepage-nav">
         <Nav className="ms-auto my-2 column-gap-25 row-gap-3">
           <Button className='ms-auto' variant="outline-primary">Create Account</Button>
           <Button className='ms-auto' variant="primary">Log In</Button>
         </Nav>
-        {/* <Nav className="ms-auto my-2 column-gap-25 row-gap-3">
-          <Nav.Link className="ms-auto"><Button variant="outline-primary">Create Account</Button></Nav.Link>
-          <Nav.Link className="ms-auto"><Button variant="primary">Log In</Button></Nav.Link>
-        </Nav> */}
       </Navbar.Collapse>
     </Navbar>
   );
