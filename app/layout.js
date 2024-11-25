5f0376e0967d7cb8be1c2b79d005f4fdde9fec3f
Jakin Stahl <jakinstahl@gmail.com>
2024-11-24 19:41:31 -0700

    added some files back

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
-import "./globals.css";
+// import "./globals.css";
+import './scss/custom.scss';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
