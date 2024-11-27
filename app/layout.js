b08b2367d91cee84ba0ee3e9fe1411c5c3e2e628
Jakin Stahl <jakinstahl@gmail.com>
2024-11-27 11:44:14 -0700

    Added next/font

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,9 @@
 import './scss/custom.scss';
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+  subsets: ['latin']
+})
 
 export const metadata = {
   title: "SpanishDex",
@@ -7,7 +12,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html className={montserrat.className} lang="en">
       <body>
         {children}
       </body>
