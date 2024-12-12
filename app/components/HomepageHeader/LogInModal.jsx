a009be0aced700631b5be0010e225ad052ba76ea
Jakin Stahl <jakinstahl@gmail.com>
2024-12-11 19:45:42 -0700

    Mostly customized modals

--- app/components/HomepageHeader/LogInModal.jsx
+++ app/components/HomepageHeader/LogInModal.jsx
@@ -1,22 +1,44 @@
 'use client'
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack"
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container"
 
 function LogInModal(props) {
   return (
-    <Modal show={props.show} onHide={props.handleClose}>
+    <Modal show={props.show} onHide={props.handleClose} backdrop="static" centered>
       <Modal.Header closeButton>
-        <Modal.Title>Modal heading</Modal.Title>
+        <Modal.Title as='h2'>Log In</Modal.Title>
       </Modal.Header>
-      <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={props.handleClose}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={props.handleClose}>
-          Save Changes
-        </Button>
-      </Modal.Footer>
+      <Modal.Body>
+        <Stack gap={5}>
+          {/* <h2>Log In</h2> */}
+          <p>Log in to SpanishDex with your existing account.</p>
+          <Form>
+            <Form.Group className="mb-5" controlId="userName">
+              <Form.Label className="fw-medium">Username or Email</Form.Label>
+              <Form.Control type="text" placeholder="username or email" />
+            </Form.Group>
+            <Form.Group className="mb-5" controlId="userEmail">
+              <Form.Label className="fw-medium">Password</Form.Label>
+              <Form.Control type="password" placeholder="password" />
+              <p style={{marginTop: '0.3125rem', textAlign: 'right'}}>
+                <a href="#">Forgot Password?</a>
+              </p>
+            </Form.Group>
+            <Container fluid className="d-flex gap-4 justify-content-end p-0">
+              <Button variant="gray" onClick={props.handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={props.handleClose}>
+                Log In
+              </Button>
+            </Container>
+          </Form>
+          <p>Don’t have an account? <a href='#'>Sign Up</a></p>
+        </Stack>
+      </Modal.Body>
     </Modal>
   );
 }
