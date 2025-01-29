effabb3f2b7f4d5b0de92e2d2ebe733fc4051389
Jakin Stahl <jakinstahl@gmail.com>
2025-01-29 07:40:26 -0700

    All functionaly of contact form is complete

--- app/components/HomepageContactForm.jsx
+++ app/components/HomepageContactForm.jsx
@@ -94,6 +94,8 @@ function HomepageContactForm(props) {
     })
   }
 
+
+  const [serverError, setServerError] = useState(''); // Holds the raw version of the server error. (stored in a hidden paragraph for debugging purposes)
   
   // Function to handle the contact form submission
   const submitContactForm = async (e) => {
@@ -117,18 +119,34 @@ function HomepageContactForm(props) {
 
     try {
       // Query the contact API
+      const response = await fetch('/api/contact', {
+        method: 'POST',
+        contentType: 'application/json',
+        body: JSON.stringify({
+          name: formValues.name.value,
+          email: formValues.email.value,
+          comment: formValues.comments.value,
+        })
+      })
 
-      // throw('error')
+      // Check if the response is ok. If it is not, AND if the response is in JSON, throw the error found in the JSON object.
+      if (!response.ok && response.headers.get('content-type') === 'application/json') {
+        const json = await response.json();
+        setServerError(JSON.stringify(json.serverError));
+        throw(json.error);
+      } else if (!response.ok) { // Otherwise, throw a generic error message based off the response status.
+        throw('Message failed to send. Please try again later. Error: ' + response.status + '.');
+      }
 
       // Update the form state to show that the form was successfully submitted
-      setFormState((prev) => ({...prev, showSuccess: true, successAcknowledged: false}))
+      setFormState((prev) => ({...prev, serverError: false, showSuccess: true, successAcknowledged: false}))
 
       // Reset the form values
       resetForm();
 
     } catch (error) {
       // Update the form state to show that an error occurred
-      setFormState((prev) => ({...prev, serverError: true, serverMessage: 'temp', errorAcknowledged: false}))
+      setFormState((prev) => ({...prev, serverError: true, serverMessage: error.toString(), errorAcknowledged: false}))
     } finally {
       // Update the form state to show that the form is no longer loading
       setFormState((prev) => ({...prev, isLoading: false}))
@@ -145,6 +163,7 @@ function HomepageContactForm(props) {
         <Alert.Heading>Success</Alert.Heading>
         Your message has been sent successfully.
       </Alert>
+      <p className="d-none text-break hiddenError">{serverError}</p>
       <Form.Group className="mb-20" controlId="userName">
         <Form.Label className="fw-medium">Name</Form.Label>
         <Form.Control type="text" placeholder="Enter your name" value={formValues.name.value} className={formValues.name.valid === false && 'is-invalid'} onChange={ updateNameValue } onBlur={(e)=>validateName(e.target.value)}/>
@@ -168,7 +187,7 @@ function HomepageContactForm(props) {
       </Form.Group>
       <Container fluid className='d-flex justify-content-center'>
         <Button variant="primary" type="submit" onClick={submitContactForm} disabled={formState.isLoading || !(formValues.name.valid && formValues.email.valid && formValues.comments.valid)}>
-            Submit
+          {formState.isLoading ? <div style={{padding: '0rem 1rem'}}><div className="loader"></div><span className="visually-hidden">Loading...</span></div> : 'Submit'}
         </Button>
       </Container>
     </Form>
