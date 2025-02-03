9a4f12578409ba4081bbe21a5bc7e4ccb5225941
Jakin Stahl <jakinstahl@gmail.com>
2025-02-03 12:17:14 -0700

    Fixed small import statement bug

--- app/(with-homepage-header)/page.js
+++ app/(with-homepage-header)/page.js
@@ -1,4 +1,3 @@
-import Homepage from "../Homepage";
 import HomepageSection from '@/components/HomepageSection'
 import HomepageSectionColumns from '@/components/HomepageSectionColumns'
 import HomepageContactForm from '@/components/HomepageContactForm'
