cfcc1e3410d49433e78655b4cd9715dd9114b23f
Jakin Stahl <jakinstahl@gmail.com>
2025-04-07 12:58:07 -0700

    Adjusted padding on the homepage for mobile layouts

--- components/HomepageSection.jsx
+++ components/HomepageSection.jsx
@@ -3,9 +3,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-function HomepageSection({children, py, backgroundColor, ...otherProps}) {
+function HomepageSection({children, py, py_sm, backgroundColor, ...otherProps}) {
     return (
-        <Container {...otherProps} fluid as='section' className={`px-5 py-${py} bg-${backgroundColor} ${otherProps.className ? otherProps.className : ''}`}>
+        <Container {...otherProps} fluid as='section' className={`px-2 px-xs_sm-5 py-${py} py-sm-${py_sm || py} bg-${backgroundColor} ${otherProps.className ? otherProps.className : ''}`}>
             <Row className='justify-content-center g-5 mx-auto' style={{maxWidth: '80rem'}}>
                 {React.Children.map(children, (child) => {
                     return <Col xs='12' sm='11' md='10' lg='12' className='d-flex justify-content-center'>
