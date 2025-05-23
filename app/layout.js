50df350366cfcab2a77325ccbfccd7dc4ca4f687
Jakin Stahl <jakinstahl@gmail.com>
2025-05-23 12:41:14 -0700

    Removed new relic code

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,6 @@ import './scss/custom.scss';
 import { Montserrat } from 'next/font/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import Script from 'next/script';
-import newrelic from 'newrelic'
 
 const montserrat = Montserrat({
   subsets: ['latin']
@@ -14,23 +13,8 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-  if (newrelic.agent.collector.isConnected() === false) {
-    await new Promise((resolve) => {
-      newrelic.agent.on("connected", resolve)
-    })
-  }
-
-  const browserTimingHeader = newrelic.getBrowserTimingHeader({
-    hasToRemoveScriptWrapper: true,
-    allowTransactionlessInjection: true,
-  })
-
   return (
     <html className={montserrat.className} lang="en">
-      <Script
-        id="nr-browser-agent"
-        dangerouslySetInnerHTML={{ __html: browserTimingHeader }}
-      />
       <body>
         {children}
         <SpeedInsights />
